ce34c63b268e5f3b586d1dfa8ef56c926f657dbc
Hanqing Zhu <hanqing.zhu@hotmail.com>
2015-01-27 15:44:10 -0600

    Add R script for ggplot excercises

---
+++ ggplot_exercises.R
@@ -0,0 +1,41 @@
+install.packages("ggplot2", dependencies=TRUE)
+install.packages("plyr")
+install.packages("ggthemes")
+install.packages("reshape2")
+libraray("ggplot2")
+library("ggplot2")
+library("plyr")
+library("ggthemes")
+library("reshape2")
+head(iris)
+iris[1:2, ]
+df <- melt(iris, id.vars = "Species")
+df[1:2, ]
+myplot <- ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width))
+summary(myplot)
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width)) + geom_point()
+
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width, color=Species)) + geom_point(aes( shape = Species), size=3)
+d2 <- diamonds[sample(1:dim(diamonds)[1],1000),]
+d2 <- diamonds[sample(1:dim(diamonds[1], 1000), ]
+ggplot(data=d2, aes(x= carat, y=price, color=color))+geom_point(size=3)
+library(MASS)
+ggplot(birthwt, aes(factor(race),bwt)) + geom_boxplot() 
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color =Species)) + geom_point() + facet_grid(. ~ Species)
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color =Species)) + geom_point() + facet_wrap( ~ Species)
+df <- melt(iris, id.vars = "Species")
+ggplot(df, aes(Species, value, fill=variable))+geom_bar(stat= "identity", position ="dodge")+scale_fill_brewer(palette ="Set1")
+ggplot(birthwt, aes(factor(race),bwt)) + geom_boxplot() 
+h <- ggplot(faithful, aes(x=waiting))
+h + geom_histogram(binwidth=8, fill = "steelblue")
+d2 <- diamonds[sample(1:dim(diamonds[1], 1000), ]
+d2 <- diamonds[sample(1:dim(diamonds)[1],1000),]
+head(d2)
+tail(df)
+ggplot(d2, aes(clarity, fill = cut))+geom_bar(position ="dodge")
+ggplot(d2, aes(clarity, carat, fill = cut))+geom_bar(stat= "identity", position ="dodge", color ="Black")
+ggplot(d2, aes(clarity, color, fill = cut))+geom_bar(stat= "identity", position ="dodge", color ="Black")
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color =Species)) + geom_point() + facet_wrap( ~ Species)
+ggplot(faithful, aes(waiting)) + geom_density(fill = "purple", alpha = 0.5)
+ggplot(iris, aes(Sepal.Length, Sepal.Width, color = Species)) + geom_point(aes(shape = Species), size = 3) + geom_smooth(method = "lm")+ facet_grid( ~ Species)
+?theme()
